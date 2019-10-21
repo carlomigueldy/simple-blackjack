682147b714bf6d8dd34a33ca38595141e7a0be5f
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-10-21 11:43:04 +0800

    refactored more

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -1,52 +1,51 @@
 import java.util.Scanner;
 
 public class Blackjack {
-	private String playerName;
-	private int playerHand;
-	private int dealerHand;
-	private boolean playerStand = true;
-	private boolean dealerStand = true;
-	private int playerChoice;
-	private Scanner playerInput = new Scanner(System.in);
-	private static Blackjack game = new Blackjack(); 
+	private static String playerName;
+	private static int playerHand;
+	private static int dealerHand;
+	private static boolean playerStand = true;
+	private static boolean dealerStand = true;
+	private static int playerChoice;
+	private static Scanner playerInput = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		game.startGame();
-		game.playerTurn();
-		game.dealerTurn();
+		startGame();
+		playerTurn();
+		dealerTurn();
 	}
 	
 	/**
-	 * Initialize the Blackjack game.
+	 * Initialize the Blackjack 
 	 */
-	public void startGame() {
+	public static void startGame() {
 		System.out.println("* Welcome to the game of Simple Blackjack!");
 		System.out.println("> Please enter your name:");
-		game.playerName = game.playerInput.nextLine();
+		playerName = playerInput.nextLine();
 		System.out.println("* Dealer deals you two cards.");
-		game.playerHand = game.dealCards();
-		game.dealerHand = game.dealCards();
-		System.out.println("* The current value of your cards on hand is " + game.playerHand + ".");
+		playerHand = dealCards();
+		dealerHand = dealCards();
+		System.out.println("* The current value of your cards on hand is " + playerHand + ".");
 	}
 	
 	/**
 	 * Player's turn to play, to 
 	 * choose between Hit or Stand.
 	 */
-	public void playerTurn() {
-		while(game.playerStand) {
+	public static void playerTurn() {
+		while(playerStand) {
 			System.out.println("> Hit (1) or Stand (0)?");
-			game.playerChoice = game.playerInput.nextInt();
-			if(game.playerChoice == 1 || game.playerChoice == 0) {
+			playerChoice = playerInput.nextInt();
+			if(playerChoice == 1 || playerChoice == 0) {
 				
-				if(game.playerChoice == 0) {
-					game.playerStand = false;
+				if(playerChoice == 0) {
+					playerStand = false;
 				} else {
 					System.out.println("* Dealer draws a card from the deck of cards and passes it to you.");
-					game.playerHand = game.playerHand + game.hit();
-					System.out.println("* The current value of your hand is " + game.playerHand + ".");	
-					if(game.playerHand > 21) {
-						System.out.println("* You just lost the game.");
+					playerHand = playerHand + hit();
+					System.out.println("* The current value of your hand is " + playerHand + ".");	
+					if(playerHand > 21) {
+						System.out.println("* You just lost the ");
 						break;
 					}
 				}
@@ -60,23 +59,23 @@ public class Blackjack {
 	/**
 	 * Dealer's turn to play.
 	 */
-	public void dealerTurn() {
-		if(!game.playerStand) {
-			System.out.println("* The current value of Dealer's hand is " + game.dealerHand + ".");
+	public static void dealerTurn() {
+		if(!playerStand) {
+			System.out.println("* The current value of Dealer's hand is " + dealerHand + ".");
 			
-			if(game.dealerHand > 21) {
-				System.out.println("* You won the game! Congratulations " + game.playerName + ".");
+			if(dealerHand > 21) {
+				System.out.println("* You won the game! Congratulations " + playerName + ".");
 			}
 			
-			while(game.dealerStand) {
+			while(dealerStand) {
 				System.out.println("* Dealer draws a card from the deck.");
-				game.dealerHand = game.dealerHand + game.hit();
-				System.out.println("* The current value of Dealer's cards is " + game.dealerHand + ".");
+				dealerHand = dealerHand + hit();
+				System.out.println("* The current value of Dealer's cards is " + dealerHand + ".");
 				
-				if(game.dealerHand > 21) {
-					System.out.println("* You won the game! Congratulations " + game.playerName + ".");
+				if(dealerHand > 21) {
+					System.out.println("* You won the game! Congratulations " + playerName + ".");
 					break;
-				} else if (game.dealerHand > game.playerHand) {
+				} else if (dealerHand > playerHand) {
 					System.out.println("* The dealer won. You lost!");
 					break;
 				}
@@ -88,7 +87,7 @@ public class Blackjack {
 	 * The dealer deals a random card.
 	 * @return random int (between 1-10)
 	 */
-	public int hit() {
+	public static int hit() {
 		int randomCard = (int) (Math.random() * 10 + 1);
 		return randomCard;
 	}
@@ -97,7 +96,7 @@ public class Blackjack {
 	 * The dealer deals two cards.
 	 * @return int
 	 */
-	public int dealCards() {
+	public static int dealCards() {
 		int dealCards = hit() + hit();
 		return dealCards;
 	}
